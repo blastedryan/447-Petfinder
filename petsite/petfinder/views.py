ea55e5f028ed49b5d38773d56fd72a4780207ec9
Jordan Troutman <jttroutman99@gmail.com>
2020-10-16 17:26:23 -0400

    Integrate CSS accordion into Django

--- petsite/petfinder/views.py
+++ petsite/petfinder/views.py
@@ -4,5 +4,5 @@ from django.template import loader
 
 # Create your views here.
 def index(request):
-    template = loader.get_template('petfinder/index.html')
+    template = loader.get_template('petfinder/Petfinder_style.html')
     return HttpResponse(template.render({}, request))
