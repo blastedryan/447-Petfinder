058f70a07bd29355466dd9fb9f943a5570fffa4b
Jordan Troutman <jttroutman99@gmail.com>
2020-10-23 05:48:54 -0400

    Display search query on webpage after search is pressed

--- petsite/petfinder/views.py
+++ petsite/petfinder/views.py
@@ -4,5 +4,8 @@ from django.template import loader
 
 # Create your views here.
 def index(request):
+    # Form requests as a dictionary
+    query = {k: v for k,v in request.GET.lists()}
     template = loader.get_template('petfinder/Petfinder_style.html')
-    return HttpResponse(template.render({}, request))
+
+    return HttpResponse(template.render({"search_query":query}, request))
