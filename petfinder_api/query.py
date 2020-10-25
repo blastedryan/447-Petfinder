3c7abb5207ab80b842195ef8294d53300cdc57c7
npotteig <npottei1@umbc.edu>
2020-10-25 18:10:02 -0400

    Added a workaround for faulty organization searches where the org name was in the database but the API could not find the org based on the full org name inputted. Slicing was used to search for a partial org name.

--- petfinder_api/query.py
+++ petfinder_api/query.py
@@ -7,6 +7,7 @@ Credit goes to https://github.com/aschleg/petpy for Petfinder Python Wrapper
 """
 import os
 from petpy.api import Petfinder
+from pandas import Series
 
 
 key = os.environ.get('PETFINDER_KEY')
@@ -38,6 +39,7 @@ Currently only 100 pets max will be returned but in the future more can be retur
 def find_pets(pf: Petfinder, location=None, animal_type=None, breed=None, size=None, gender=None, age=None, color=None,
               coat=None, org_name=None, distance=None, name=None, good_with=[], house_trained=None, special_needs=None,
               sort=None):
+    # Create a boolean list of values related to what the animal is good_with
     actual_compatible = [None, None, None]
     possible_compatible = ['cat','dog', 'children']
     if len(good_with) != 0:
@@ -48,18 +50,29 @@ def find_pets(pf: Petfinder, location=None, animal_type=None, breed=None, size=N
     org_ids = None
     if org_name is not None:
         """
-        To do: Create a method that searches for the full name of an org. If the name cannot be found, slice the string
-        and retry the search with the sliced string. Find the search result in the dataframe by checking name column
-        value.
+        If the org name is not found in the database, meaning a possible faulty search occurred, slice words from the
+        front of the org name until either organization ids are returned or none are. The organization ids returned will 
+        be used to search for pets within those organizations.
         """
-        org_ids = __get_org_ids(pf, org_name)
-        if not org_ids:
+        tmp_name = org_name
+        org_ids = __get_org_ids(pf, orgname=tmp_name)
+        if not isinstance(org_ids, Series):
+            length = len(tmp_name.split()) - 1
+            org_bool = False
+            for i in range(length):
+                tmp_name = tmp_name.split(' ', 1)[1]
+                org_ids = __get_org_ids(pf, orgname=tmp_name)
+                if isinstance(org_ids, Series):
+                    org_bool = True
+                    break
+
+        if not org_bool:
             return 0
     try:
         pets = pf.animals(location=location, animal_type=animal_type, breed=breed, size=size, gender=gender, age=age,
                       color=color, coat=coat, distance=distance, name=name, good_with_cats=actual_compatible[0],
                       good_with_dogs=actual_compatible[1], good_with_children=actual_compatible[2],
-                      results_per_page=100, organization_id=org_ids, pages=1, sort=sort, return_df=True)
+                      results_per_page=50, organization_id=org_ids, pages=1, sort=sort, return_df=True)
     except:
         return 0
 
@@ -74,18 +87,16 @@ Returns a pandas dataframe of organization ids to be used in find_pets() to find
 Input: A string representing partial or full organization name
 Output: The organization ids associated with the string or 0 if no org could be found / error occurred
 
-The output can fail depending on the string inputted even if the organization is in a database.
-Will look further into the cause of this. An example 'UC Davis Dog and Cat Adoptions' can be found by searching the
-string 'Davis Dog and Cat Adoptions' but fails to find it with the full string.
+The output can fail depending on the string inputted even if the organization is in a database. 
 
 The only consistent output is when only one word is inputted
 """
 def __get_org_ids(pf: Petfinder, orgname=None):
     try:
         orgs = pf.organizations(name=orgname, results_per_page=100, pages=1, return_df=True)
-        print(orgs['name'][0:5])
-        print(orgs.shape)
+        # print(orgs['name'][0:5])
+        # print(orgs.shape)
         return orgs['id']
     except:
-        print('Failed')
+        # print('Failed')
         return 0
