8c9373c9016672babe18f2bf23ef17b6e57dbb64
npotteig <npottei1@umbc.edu>
2020-10-28 16:14:49 -0400

    Added the ability to write and read a json which represents the search query and a short test to test the functionality of reading the json and searching for pets.

--- petsite/petfinder/views.py
+++ petsite/petfinder/views.py
@@ -1,11 +1,14 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.template import loader
+import json
 
 # Create your views here.
 def index(request):
     # Form requests as a dictionary
     query = {k: v for k,v in request.GET.lists()}
+    with open('search.json', 'w') as fp:
+        json.dump(query, fp)
     template = loader.get_template('petfinder/Petfinder_style.html')
 
     return HttpResponse(template.render({"search_query":query}, request))
