e504f41724e42e3f4f3568d60d81c08c098a9b5b
blastedryan <31160332+blastedryan@users.noreply.github.com>
2020-11-02 17:51:37 -0500

    added tests and more fixes

---
+++ tests/test_cache.py
@@ -0,0 +1,35 @@
+from petfinder_api.query import find_pets, authenticate, key, secret_key
+from petfinder_api.cache import find_pets_with_cache
+from pandas import DataFrame
+from django.core.cache import cache
+import pytest
+
+pf = authenticate(key, secret_key)
+num_results = 100 #number of results we want
+
+def test_petfind_with_cache():
+    cache.clear() #make sure we start with an empty cache
+    pets = find_pets_with_cache(pf)
+    cache_result = cache.get('default')
+    assert isinstance(cache_result, DataFrame)
+    assert cache_result.shape[0] == num_results
+    assert cache_result == pets
+
+def test_petfind_complex_with_cache():
+    cache.clear()  # make sure we start with an empty cache
+    pets = find_pets_with_cache(pf, location='Baltimore, MD', animal_type='dog', breed='beagle', distance=50, name='Bayla',
+                     age='young', size='small', gender='female', coat='short')
+    cache_result = cache.get('default')
+    assert isinstance(pets, DataFrame)
+    assert cache_result['name'][0] == 'Bayla'
+
+
+
+
+
+
+
+
+
+
+
