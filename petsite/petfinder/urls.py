819dba60413d7269afc204fb3d3443ad8c25febf
JimKInter123 <hq25495@umbc.edu>
2020-11-16 16:28:01 -0500

    Added remaining webpages with most of the images added

--- petsite/petfinder/urls.py
+++ petsite/petfinder/urls.py
@@ -8,5 +8,8 @@ urlpatterns = [
     url(r'^cats_request/$', views.cats_request, name = 'Cats'),
     url(r'^birds_request/$', views.birds_request, name='Birds'),
     url(r'^rabbits_request/$', views.rabbits_request, name = 'Rabbits'),
-    url(r'^scales_request/$', views.scales_request, name='Scales')
+    url(r'^scales_request/$', views.scales_request, name='Scales'),
+    url(r'^horse_request/$', views.horse_request, name='Horse'),
+    url(r'^barn_request/$', views.barn_request, name='Barn'),
+    url(r'^furry_request/$', views.furry_request, name='Furry')
 ]
