7ebe2b5940ca982a2922183dc7fb8191cfd18143
Jordan Troutman <jttroutman99@gmail.com>
2020-11-30 21:27:20 -0500

    Generate menu items with DB

--- petsite/petfinder/admin.py
+++ petsite/petfinder/admin.py
@@ -1,7 +1,9 @@
 from django.contrib import admin
 
-from .models import Animal, Search
+from .models import Animal, Search, Card, CardItem
 
 # Register your models here.
 admin.site.register(Animal)
 admin.site.register(Search)
+admin.site.register(Card)
+admin.site.register(CardItem)
