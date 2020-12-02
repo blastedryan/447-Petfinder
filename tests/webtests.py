e9be721550a88a30750857f1ab383a0837cf2db2
npotteig <npottei1@umbc.edu>
2020-12-02 15:56:33 -0500

    Added environ variable for chromedriver path

--- tests/webtests.py
+++ tests/webtests.py
@@ -2,14 +2,20 @@ import unittest
 import subprocess
 import os
 from selenium import webdriver
+
 class WebTests(unittest.TestCase):
     def setUp(self):
         port = '8000'
         driverPath = '/Users/monalisaraf/Downloads/chromedriver'
+        chrome_options = webdriver.ChromeOptions()
+        chrome_options.binary_location = os.environ.get("GOOGLE_CHROME_BIN")
+        chrome_options.add_argument("--headless")
+        chrome_options.add_argument("--disable-dev-shm-usage")
+        chrome_options.add_argument("--no-sandbox")
         manage_py_path = os.path.join(os.path.dirname((os.path.dirname(os.path.abspath(__file__)))), 'petsite/manage.py')
         self.process = subprocess.Popen(['python', manage_py_path , 'runserver', port])
         self.htmlPath = 'http://127.0.0.1:{}/petfinder'.format(port)
-        self.driver = webdriver.Chrome(driverPath)
+        self.driver = webdriver.Chrome(executable_path=os.environ.get("CHROME_DRIVER_PATH"))
 
     def tearDown(self):
         self.process.kill()
