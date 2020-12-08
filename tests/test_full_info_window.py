bd712fdf524a1cbff5e31b883e9507b8c2e2c7f0
monalisa219 <msaraf219@gmail.com>
2020-12-07 19:49:15 -0500

    added testing and editted the README

---
+++ tests/test_full_info_window.py
@@ -0,0 +1,34 @@
+from tests.webtests import WebTests
+from selenium.webdriver.support.ui import Select
+import time
+import os
+import json
+from selenium.webdriver import ActionChains
+
+
+class UpdatingMap(WebTests):
+
+    def test_search(self):
+
+        
+        self.driver.get(self.htmlPath)
+        time.sleep(2)
+        self.driver.find_element_by_id("dog_button").click()
+        time.sleep(2)
+        self.driver.find_element_by_id('headingOne').click()
+        time.sleep(2)
+        self.driver.find_element_by_xpath("//input[@name='breed']").send_keys("Boxer")
+        
+        time.sleep(2)
+        self.driver.find_element_by_xpath("//input[@name='location']").send_keys("Baltimore, MD")
+
+        time.sleep(2)
+        submit = self.driver.find_element_by_xpath("//input[@value='search']")
+        submit.click()
+        
+        action = ActionChains(self.driver)
+        map_elm = self.driver.find_element_by_id('map')
+        action.move_to_element(map_elm).perform()
+
+        time.sleep(10)
+
